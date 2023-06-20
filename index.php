fce80c11f8b582ac3f642d6fb12b9a700d512e42
Huseyin-01 <h.tufekci@student.avans.nl>
2023-06-20 17:09:45 +0200

    Add application initial files

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$page_title = "home"
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title; ?></title>
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
